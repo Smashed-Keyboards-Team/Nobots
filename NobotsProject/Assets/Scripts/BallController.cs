0113f5bd5f40a6d59a9e6da7245c373edc787dab
TheAvaro <TheAvaro@users.noreply.github.com>
2019-11-14 03:50:02 +0100

    siestonovamañanatevanadarporculonico

---
+++ NobotsProject/Assets/Scripts/BallController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+	private float moveHorizontal;
+	private float moveVertical;
+	private Vector2 axis;
+	private Rigidbody rb;
+	private Vector3 movement;
+
+	private Vector3 moveDirection;
+    private Vector3 desiredDirection;
+
+	public float speed;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update ()
+	{
+		desiredDirection = transform.right * axis.x * speed;
+
+        /*
+		moveDirection = new Vector3(desiredDirection.x, 
+            moveDirection.y,
+            desiredDirection.z);
+			*/
+	}
+
+    void FixedUpdate ()
+	{
+		//Movimiento
+		
+		axis.x = Input.GetAxis("Horizontal");														
+		axis.y = Input.GetAxis("Vertical");
+
+		//movement = new Vector3 (axis.x, 0.0f, axis.y);
+		movement = new Vector3 (desiredDirection.x, 0.0f, desiredDirection.z);
+
+		rb.AddForce(movement * speed);
+
+		
+	}
+}
